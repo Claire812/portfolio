105c5cac5b7015eca05019fa12f612070e3fb0b7
Claire812 <clairedaudignon@gmail.com>
2021-02-04 14:30:59 +0100

    add a search for project

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Form\SearchProjectFormType;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/project", name="project_")
@@ -16,14 +18,26 @@ class ProjectController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(ProjectRepository $projectRepository): Response
+    public function index(Request $request,ProjectRepository $projectRepository): Response
     {
         $projects = $projectRepository->findAll();
 
+        $formSearch = $this->createForm(SearchProjectFormType::class);
+        $formSearch->handleRequest($request);
+
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $search = $formSearch->getData()['search'];
+            $projects = $projectRepository->findLikeName($search);
+        } else {
+            $projects = $projectRepository->findAll();
+        }
+
         return $this->render('project/index.html.twig', [
             'projects' => $projects,
+            'form' => $formSearch->createView(),
         ]);
     }
+
 
 
 }
